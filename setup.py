a185fbab37cb300b6addb4b083dd55c6019d17d5
AHAPX <anarchy.b@gmail.com>
2018-03-04 07:02:45 +0000

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name="pymaker",
+    version="0.5",
+    description="pymaker package",
+    author="MakerDao",
+    url="https://github.com/makerdao/pymaker",
+    packages=["pymaker"]
+)
